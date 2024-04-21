48329d75f70fdf1a79e962df0b66ac1ffebbfcda
mangosteen977 <mosubin977@daum.net>
2024-04-21 22:32:48 +0900

    [framer-motion]  AnimatePresence의  onExitComplete(exit이 종료시 동작), initial(로딩시 initial 동작 실행 여부) props 사용, offset함수로 배열 6개씩 슬라이딩 포스터 만들기

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,9 +2,12 @@ import styled from "styled-components";
 import { useQuery } from "react-query";
 import { IGetMovieNowPlayingResult, getMovieNowPlaying } from "../api";
 import { makeImagePath } from "../utilis";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background-color: black;
+  overflow-x: hidden;
 `;
 
 const Loader = styled.div`
@@ -14,14 +17,14 @@ const Loader = styled.div`
   align-items: center;
 `;
 
-const Banner = styled.div<{ bgPhoto: string }>`
+const Banner = styled.div<{ $bgPhoto: string }>`
   height: 100vh;
   padding: 60px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${(props) => props.bgPhoto});
+    url(${(props) => props.$bgPhoto});
   background-size: cover;
 `;
 
@@ -35,25 +38,94 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  position: absolute;
+`;
+const Box = styled(motion.div)<{ $bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.$bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  font-size: 70px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth,
+  },
+  visible: { x: 0 },
+  exit: { x: -window.outerWidth },
+};
+
+const offset = 6;
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMovieNowPlayingResult>(
     ["movies", "nowPlaying"],
     getMovieNowPlaying
   );
-  console.log(data);
-  // 이미지 https://image.tmdb.org/t/p
-  // https://image.tmdb.org/t/p/original/ 원본 크기
-  // https://image.tmdb.org/t/p/w500/ width지정
+  // console.log(data);
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={increaseIndex}
+            $bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              {/*
+              onExitComplete : AnimatePresence의 props, exit이 종료시 동작 
+              initial : false일 경우, 웹페이지 처음 로딩 시, 하위 객체의 initial 동작을 실행하지 않음.
+              */}
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      $bgPhoto={makeImagePath(movie.poster_path || "")}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
